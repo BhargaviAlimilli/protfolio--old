30e24c1240df7dac8b9ea74216d22b21a832af60
BhargaviAlimilli <78538578+BhargaviAlimilli@users.noreply.github.com>
2022-04-21 13:21:49 +0530

    project technologies update

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,11 +9,11 @@ const PROJECTS = [
    
     {
         id: 3,
-        title: "Quartos(MERN)",
+        title: "Book My Stay(MERN)",
        
         functionalArea: "Hotel Booking",
         technologyUsed: "Nodejs, ExpressJS, ES6 javascript,React,Mongo Atlas, Bootstrap",
-        description: "Quartos is a multi user platform with a proper authentication system were seller can host their property for users to book and users can book those properties.Users can search the properties with specific dates and occupancy capacity.Users can view the property images and book the property with online payment and the seller can view the amount he earned through hosting the properties.Stripe Payment Integration has been done",
+        description: "Book My Stay is a multi user platform with a proper authentication system where sellers can host their property and users can book those properties.Users can search the properties with specific dates and occupancy capacity.Users can view the property images and book the property with online payment and the seller can view the amount he earned through hosting the properties.Stripe Payment Integration has been done for payments, Ant Designs has been used as React UI library.",
         image: project3,
         link: "https://62432898bdf1d60084733292--benevolent-dragon-a8bf5a.netlify.app/"
     },
@@ -22,8 +22,8 @@ const PROJECTS = [
         id: 5,
         title: "Simplify-Notes",
         functionalArea: "CMS",
-        technologyUsed: "React JS, Redux, NodeJS.",
-        description: "Simplify notes is a full stack web app made with MERN Stack.Basic CRUD Was implemented using NodeJs,ReactJs and Redux, Client Side Search Functionality added.Proper authentication implemented and deployed",
+        technologyUsed: "React JS, Redux, NodeJS, ExpressJS, Mongo Atlas, React Bootstrap",
+        description: "Simplify notes is a full stack web app made with MERN Stack.Basic CRUD Was implemented using NodeJs,ReactJs and Redux, Client Side Search Functionality added.Proper authentication implemented, React Bootestrap and Bootswatch themes has been used for UI designing and styling.",
         image: project1,
         link: "https://625d70c472983056337578de--hilarious-starship-52f2b6.netlify.app/"
     }   
