e826e52b898bb8e0a4268ddf00ab137bf3466ee7
BhargaviAlimilli <78538578+BhargaviAlimilli@users.noreply.github.com>
2022-04-21 13:24:13 +0530

    Update ProjectSummary

--- src/data/profileSummary.js
+++ src/data/profileSummary.js
@@ -1,3 +1,3 @@
-const PROFILE_SUMMARY = ["A Full Stack Developer who is having hands-on experience in JavaScript Technologies (Node, Express, React, ExtJs) with Database (MongoDB & PostgreSQL). I'm always ready to wear multiple hats and ready to learn new technologies."];
+const PROFILE_SUMMARY = ["A Full Stack Developer who is having hands-on experience in JavaScript Technologies (Node, Express, React) with Database (MongoDB, SQL(Basic)). I'm always ready to learn the ropes."];
 
 export default PROFILE_SUMMARY;
